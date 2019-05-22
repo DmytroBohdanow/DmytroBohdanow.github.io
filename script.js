bff61dde7b2bd5a4e6cce5b5b9d9a16033d5e255
Joker55222 <joker55222@gmail.com>
2019-05-22 23:55:32 +0200

    new effects and background added

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-    $('.upper_menu_item, .menu_list_item').hover(function() {
+    $("#greeting").delay(1500).fadeTo(2500, 1 );
+    $("#invitation").delay(3000).fadeTo(3500, 1 );
+    $('.upper_menu_item, .menu_list_item, #settings_block').hover(function() {
         $(this).stop().animate({ backgroundColor: "#A9A9A9"}, 400);
     },function() {
         $(this).stop().animate({ backgroundColor: "#D3D3D3" }, 800);
@@ -20,4 +22,11 @@ $(document).ready(function(){
             $(this).delay(600 * index).fadeOut(600);
         });
     });
+    $(window).scroll(function(){
+        if($(window).scrollTop()>60) {
+            $("#text_block").fadeTo(3000, 0.8);
+            $("#greeting, #invitation").delay(3000).fadeOut(600);
+            $("#contact_block").delay(3000).fadeTo(3000, 0.8);
+        }
+        });
     });
